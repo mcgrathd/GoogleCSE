d2033c7b27c0a4896392b47d42b163a64fd5854b
Dan McGrath <danmcgrath.ca@gmail.com>
2012-11-01 19:06:58 -0400

    Add credits to the extension

--- GoogleCSE.php
+++ GoogleCSE.php
@@ -7,4 +7,14 @@ require_once( "$IP/extensions/GoogleCSE/GoogleCSE.php" );
 EOT;
     exit( 1 );
 }
+
+$wgExtensionCredits['specialpage'][] = array(
+    'path' => __FILE__,
+    'name' => 'Google CSE',
+    'author' => 'Dan McGrath',
+    'url' => 'https://localhost/coming_soon',
+    'descriptionmsg' => 'Use MediaWiki to interface to Google Custom Search Engine (CSE)',
+    'version' => '0.0.0',
+);
+
 ?>
