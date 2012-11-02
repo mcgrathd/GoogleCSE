8edc3f3f7de40953ba4a4023b826f023e425bd49
Dan McGrath <danmcgrath.ca@gmail.com>
2012-11-01 20:14:24 -0400

    Add basic i18n info

---
+++ GoogleCSE.i18n.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Internationalisation for GoogleCSE
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author dmcgrath
+ */
+$messages['en'] = array(
+    'googlecse' => "Google CSE", // The string that appears on Special:SpecialPages
+    'googlecse-desc' => "Google Custom Search Engine",
+);
+
+/** Message documentation
+ * @author dmcgrath
+ */
+$message['qqq'] = array(
+    'googlecse' => "Google CSE",
+    'googlecse-desc' => "{{desc}}",
+);
