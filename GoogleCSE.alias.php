1768294cb14b07e88584f5648823a2bcbf1e9e4e
Dan McGrath <danmcgrath.ca@gmail.com>
2012-11-01 20:18:43 -0400

    Boiler plate for aliases

---
+++ GoogleCSE.alias.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Aliases for GoogleCSE
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author dmcgrath
+ */
+$specialPageAliases['en'] = array(
+    'GoogleCSE' => array( 'GoogleCSE', 'Google CSE' ),
+);
+
+?>
